f90cce09ff83d57010d09ddac60d153bb3434c1c
saleemabuzannad <142873566+saleemabuzannad@users.noreply.github.com>
2023-12-18 10:22:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-//Select Elements
-let countSpan = document.querySelector('.count span');
-let flagImgDiv = document.querySelector('.flag-img');
-let flagImg = document.querySelector('.flag-img img');
-let flagOptions = document.querySelector('.flag-options ul');
-let flagLis = document.querySelectorAll('.flag-options ul li');
-let score = document.querySelector('h3 span');
-let scoreDiv = document.querySelector('.score');
-let correctAns = document.querySelector('.score .right span');
-let incorrectAns = document.querySelector('.score .incorrect span');
-let btnNewGame = document.querySelector('#newGame');
 
 let currentIndex = 0;
 let rightAnswer = 0;
